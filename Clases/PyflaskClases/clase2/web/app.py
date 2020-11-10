71bf26c24d838a9d7ac5a32806cbac19bf01ba58
Danielherresan <danielherresan@gmail.com>
2020-11-10 12:59:29 -0500

    subiendo cambios en web2

--- Clases/PyflaskClases/clase2/web/app.py
+++ Clases/PyflaskClases/clase2/web/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, url_for
-
+import utilidades as helper
 # se crea un objeto del tipo app
 app = Flask(__name__)
+data = helper.leerArchivo('usuarios.csv')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -10,19 +11,33 @@ def not_found(error):
 
 @app.route('/')
 def baseRoute():
+
+
     return redirect(url_for('login'))
 
 @app.route('/home')
 def home():
    return render_template('home.html')
 
+
+@app.route('/singin', methods = ['POST','GET'])
+def singin():
+
+    return render_template('signIn.html')
+
+
+
 @app.route('/login', methods = ['POST','GET'])
 def login():
+    
     if request.method == 'POST':
         nameUser = request.form['name']
         passUser = request.form ['pass']
-        if (passUser == 'hola1234'):
+        output = helper.validatePassword (data, nameUser, passUser)
+        if (output == True):
             return  redirect(url_for('home'))
+        elif (output == 'Usuario no registrado'):
+            return  redirect(url_for('singin'))
         else:
             return 'Falló proceso de autenticación'
     else: 
